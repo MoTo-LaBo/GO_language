7bdf643eac9c79d8f8d899010849a242efb71e16
MoTo <moto.labo.desgin@gmail.com>
2021-09-17 13:36:43 +0900

    data type

--- Go_lang/02_基本型/main.go
+++ Go_lang/02_基本型/main.go
@@ -3,31 +3,32 @@ package main
 /* 型 : int, float, uint, coplex, bool, string, byte */
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 
 	/* ---------- int　(integer : 整数) ---------- */
 	// 環境依存 :　pc環境によって　int8,16,32,64 という風に数値が変わる
-	var i int = 100
+	// var i int = 100
 
 	var i2 int64 = 200
 
-	fmt.Println(i + 50)
+	// fmt.Println(i + 50)
 	// fmt.Println(i + i2) : int値が合わないと計算できない
 	// 型変換
 	fmt.Printf("%T\n", i2)
 	fmt.Printf("%T\n", int32(i2)) // 型変換：引数に渡す
 	// 型変換後
-	fmt.Println(i + int(i2))
+	// fmt.Println(i + int(i2))
 
 	/* ----------- 浮動小数点型 ( float ) ---------- */
-	var fl64 float64 = 2.4
-	fmt.Println(fl64)
+	// var fl64 float64 = 2.4
+	// fmt.Println(fl64)
 
-	fl := 3.2 // 暗黙的な定義の場合は自動で float64 ※ 環境依存では無い
-	fmt.Println(fl64 + fl)
-	fmt.Printf("%T, %T\n", fl64, fl)
+	// fl := 3.2 // 暗黙的な定義の場合は自動で float64 ※ 環境依存では無い
+	// fmt.Println(fl64 + fl)
+	// fmt.Printf("%T, %T\n", fl64, fl)
 
 	var fl32 float32 = 1.2
 	fmt.Printf("%T\n", fl32) // float32 は基本的には使用しない
@@ -60,9 +61,9 @@ func main() {
 	/* ---------- string　(string : 文字列型) ---------- */
 	// 文字列はバイト配列の集まり
 
-	var s string = "Hello Golang"
-	fmt.Printf(s)
-	fmt.Printf("%T\n", s)
+	// var s string = "Hello Golang"
+	// fmt.Printf(s)
+	// fmt.Printf("%T\n", s)
 
 	var si string = "300"
 	fmt.Printf(si)
@@ -75,8 +76,8 @@ func main() {
 	fmt.Printf("\"\n")
 	fmt.Printf(`""`)
 
-	fmt.Println("\n", s[0])         // バイト型　： 72 と表示される
-	fmt.Println("\n", string(s[0])) // バイト型 -> str型に変換　: H と表示される
+	// fmt.Println("\n", s[0])         // バイト型　： 72 と表示される
+	// fmt.Println("\n", string(s[0])) // バイト型 -> str型に変換　: H と表示される
 
 	/* ---------- byte　(uint8 : byte型) ---------- */
 
@@ -152,5 +153,56 @@ func main() {
 	// data 特有の計算や演算はできないので注意
 	// x = 2
 	// fmt.Println(x + 3)
+
+	/* -------------------- 型変換 -------------------- */
+
+	// var i int = 1
+	// fl64 := float64(i)
+	// fmt.Println(fl64)
+	// fmt.Printf("i = %T\n", i)
+	// fmt.Printf("fl64 = %T\n", fl64)
+
+	// i1 := int(fl64)
+	// fmt.Printf("i = %T", i1)
+
+	// fl := 10.5
+	// i3 := int(fl)
+	// fmt.Printf("i3 =%T\n", i3)
+	// fmt.Println(i3)           // 小数点以下が切り捨てられる
+
+	// string -> int
+	var s string = "100"
+	fmt.Printf("s =%T\n", s)
+
+	/* 文字列型 -> 数値型への変換する関数 : strconv.Atoi() -> 値を２つ返す
+	_ (アンダーバー)は、受け取った値を破棄してくれる
+	定義した変数は必ず使用するというルールがあるので,回避できる*/
+
+	i, _ := strconv.Atoi(s)
+
+	// error ハンドリング
+	// i, err := strconv.Atoi(s)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	fmt.Println(i)
+	fmt.Printf("i =%T\n", i)
+
+	// string -> int
+	// var i3 int = 200
+	// s2 := strconv.Itoa(i3)
+	// fmt.Println(s2)
+	// fmt.Printf("s2 =%T\n", s2)
+
+	// string -> byte
+	var h string = "HelloWorld"
+	b := []byte(h)
+	fmt.Println(b)
+	fmt.Printf("b = %T\n", b)
+
+	// byte -> string
+	h2 := string(b)
+	fmt.Println(h2)
+	fmt.Printf("h2 = %T\n", h2)
 
 }
